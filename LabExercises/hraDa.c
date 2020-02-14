69f8d0a820200cb4e4c771fb752e8ea25be86fbf
viruchith <58523045+viruchith@users.noreply.github.com>
2020-02-14 10:36:46 +0530

    Sp

--- LabExercises/hraDa.c
+++ LabExercises/hraDa.c
@@ -6,23 +6,23 @@ float hra,da,sp,final;
 printf("Enter basic:");
 scanf("%d",&b);
 if(b<10000){
-	da=b*(25/100);
-	hra=b*(15/100);
-	sp=b*(5/100);
-	loan=500;
-	}else if(b>=10000 && b<=50000){
-		da=b*(35/100);
-		hra=b*(25/100);
-		sp=b*(10/100);
-		loan=1000;
-		}else{
-			da=b*(50/00);
-			hra=b*(30/100);
-			sp=b*(20/100);
-			loan=1500;
-			}
-	final=da+hra+sp+b-loan;
-	printf("Final pay=₹%.2f",final);	
+da=b*(25/100);
+hra=b*(15/100);
+sp=b*(5/100);
+loan=500;
+}else if(b>=10000 && b<=50000){
+da=b*(35/100);
+hra=b*(25/100);
+sp=b*(10/100);
+loan=1000;
+}else{
+da=b*(50/00);
+hra=b*(30/100);
+sp=b*(20/100);
+loan=1500;
+}
+final=da+hra+sp+b-loan;
+printf("Final pay=₹%.2f",final);	
 }
 /*
 Output:
