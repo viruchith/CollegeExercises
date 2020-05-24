f4e9f3f7db4d4165edbad7de317b3c51de8f6d7c
Viruchith <58523045+viruchith@users.noreply.github.com>
2020-05-24 17:29:03 +0530

    Update arraySortDescend.c

--- ClassExercises/arraySortDescend.c
+++ ClassExercises/arraySortDescend.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 void main(){
-int arr[7]={4,3,7,2,6,1,5},t=0;
-int *ptr=&arr[0];
-for(int i=0;i<7;i++){
+int *arr,t=0,n;
+printf("Enter no of items : ");
+scanf("%d",&n);
+arr=(int *)calloc(n,sizeof(int));
+for(int i=0;i<n;i++){
+    printf("Enter arr[%d]",i);
+    scanf("%d",arr+i);
+}
+for(int i=0;i<n;i++){
     for(int j =0;j<7;j++){
-        if(*(ptr+i)>*(ptr+j)){
-            t=*(ptr+i);
-            *(ptr+i)=*(ptr+j);
-            *(ptr+j)=t;
+        if(*(arr+i)>*(arr+j)){
+            t=*(arr+i);
+            *(arr+i)=*(arr+j);
+            *(arr+j)=t;
         }
     }
 }
-printf("Descending order : ");
+printf("\nDescending order : ");
 for(int i =0;i<7;i++)
-    printf("%d ",*(ptr+i));
+    printf("%d ",*(arr+i));
 }
 
 /*
 Output :
-Descending order : 7 6 5 4 3 2 1*/
+
+Enter no of items : 7
+Enter arr[0]4
+Enter arr[1]3
+Enter arr[2]7
+Enter arr[3]2
+Enter arr[4]6
+Enter arr[5]1
+Enter arr[6]5
+
+Descending order : 7 6 5 4 3 2
+*/
