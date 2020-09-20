8e1e92171ca9e4490371ada1005add6fb9d2fb26
Viruchith <58523045+viruchith@users.noreply.github.com>
2020-09-20 17:31:24 +0530

    Update BankInterest.java

--- OOP/ClassExercise/BankInterest.java
+++ OOP/ClassExercise/BankInterest.java
@@ -1,3 +1,8 @@
+/*
+Consider a scenario to write a Java program where Bank is a class that provides a method to get the rate of interest. 
+However, the rate of interest may differ according to banks. 
+For example, SBI, ICICI, and AXIS banks are providing 5.2%, 4.3%, and 10.7% rate of interest.
+*/
 class Bank{
     protected double r = 0;
     protected void setRate(double r){
@@ -19,7 +24,7 @@ class AXIS extends Bank{
     
 }
 
-public class BankInterest
+public class Main
 {
 	public static void main(String[] args) {
 	    SBI sb = new SBI();
@@ -27,10 +32,18 @@ public class BankInterest
 	    AXIS ax = new AXIS();
 	    
 	    sb.setRate(5.2);
-	    System.out.println("SBI : "+sb.getInterest(10000,12));
+	    System.out.println("SBI : Rs."+sb.getInterest(10000,12));
 	    ic.setRate(4.3);
-	    System.out.println("ICICI : "+ic.getInterest(10000,12));
+	    System.out.println("ICICI : Rs."+ic.getInterest(10000,12));
 	    ax.setRate(10.7);
-	    System.out.println("Axis : "+ax.getInterest(10000,12));
+	    System.out.println("Axis : Rs."+ax.getInterest(10000,12));
 	}
 }
+
+/*Output : 
+
+SBI : Rs.6240.0                                                                                                         
+ICICI : Rs.5160.0                                                                                                       
+Axis : Rs.12840.0 
+
+*/
