1e8731d0a00f4988cbd66d333881a6096cfc196a
Viruchith <58523045+viruchith@users.noreply.github.com>
2020-11-04 18:25:58 +0530

    Update exe7.java

--- OOP/LabExercises/exe7.java
+++ OOP/LabExercises/exe7.java
@@ -1,5 +1,31 @@
 /*
+Define an interface “IntOperations” with methods to check whether an integer is positive, negative, even, odd, prime and operations like factorial and sum of digits. Define a class MyNumber having one private int data member and implement the interface. Write a default constructor to initialize it to 0 and another constructor to initialize it to a value (Use this).
 
+Hint:
+
+public static void main(String a[])
+    {
+        MyNumber m=new MyNumber(11);
+        m.integer();
+        m.evenodd();
+        m.prime();
+        m.factorial();
+        m.sumofdigit();
+    }
+
+Output:
+
+11 is a Positive Number
+11 is a Odd Number
+11 is a Prime Number
+The factorial of 11 is 39916800
+Sum of it's digits is 2
+
+14 is a Positive Number
+14 is a Even Number
+14 is not a Prime Number
+The factorial of 14 is 1278945280
+Sum of it's digits is 5
 */
 interface IntOperations{
     public void factorial();
