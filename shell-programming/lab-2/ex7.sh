8f6a38fb65b40babe4f6c1450190d587d5f15c4b
Viruchith <58523045+viruchith@users.noreply.github.com>
2021-03-25 09:48:02 +0530

    Update ex7.sh

--- shell-programming/lab-2/ex7.sh
+++ shell-programming/lab-2/ex7.sh
@@ -1,21 +1,21 @@
 # Greet the user based on time
-
+  
 USERNAME=`whoami`
 
 HOUR=`date "+%H"`
 
-if (( HOUR >= 5 && HOUR < 12 ))
+if [ $HOUR -lt 12 ]
 # 5:00 AM — 11:59 AM
 then
-	echo "Good Morning, $USERNAME."
+        echo "Good Morning, $USERNAME."
 
-elif (( HOUR >= 12 && HOUR < 17 ))
+elif [ $HOUR -lt 17 ]
 #12:00 PM — 4:59 PM
-then 
-	echo "Good Afternoon, $USERNAME."
+then
+        echo "Good Afternoon, $USERNAME."
 #5:00 PM — 4:59 AM
 else
-	echo "Good Evening, $USERNAME."
+        echo "Good Evening, $USERNAME."
 fi
 
 
