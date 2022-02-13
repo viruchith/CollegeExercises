135ce9a7ad57670a975e0df6fe0a2fc36db9eebb
Viruchith <58523045+viruchith@users.noreply.github.com>
2022-02-13 16:43:43 +0530

    Update StringPrograms.java

--- Java/StringPrograms.java
+++ Java/StringPrograms.java
@@ -3,8 +3,7 @@
 *
 */
 
-// How to Print duplicate characters
-from String?
+// How to Print duplicate characters from String?
 
 import java.util.*;
 
@@ -28,3 +27,80 @@ public class Main
 		System.out.println("Duplicate Characters : "+duplicates);
 	}
 }
+
+
+--------------------------------------------------------------------------------------------------------
+// How to check if two Strings are anagrams of each other?
+
+	
+import java.util.*;
+
+public class Main
+{
+    
+    public static boolean isAnagramStringBuilder(String word,String anagram){
+        
+        if(word.length()!=anagram.length()){
+            return false;
+        }
+		
+		StringBuilder sb = new StringBuilder(anagram);
+		
+		for(String letter : word.split("")){
+		    int index = sb.indexOf(letter);
+		    if(index==-1){
+		        break;
+		    }
+		    sb.deleteCharAt(index);
+		}
+		
+		return sb.length()==0;
+    }
+    
+    public static boolean isAnagramHashMap(String word,String anagram){
+        if(word.length()!=anagram.length()){
+            return false;
+        }
+        
+        Map<Character,Integer> wordMap = new HashMap<>() , anagramMap = new HashMap<>();
+        
+        for(int i = 0 ; i<word.length() ; i++){
+            Character wordLetter = word.charAt(i) , anagramLetter = anagram.charAt(i);
+            
+            if(wordMap.containsKey(wordLetter)){
+                wordMap.put(wordLetter,wordMap.get(wordLetter)+1);
+            }else{
+                wordMap.put(wordLetter,1);
+            }
+            
+            if(anagramMap.containsKey(anagramLetter)){
+                anagramMap.put(anagramLetter,wordMap.get(anagramLetter)+1);
+            }else{
+                anagramMap.put(anagramLetter,1);
+            }
+        }
+        
+        return wordMap.equals(anagramMap);
+    }
+    
+	public static void main(String[] args) {
+		String word1 = "mary" , anagram1 = "army";
+		String word2 ="hello" , anagram2 = "elhlo";
+		String word3 ="jim" , anagram3 = "jam";
+    
+        System.out.println(isAnagramHashMap(word1,anagram1));
+        System.out.println(isAnagramHashMap(word3,anagram3));
+        
+        System.out.println(isAnagramStringBuilder(word1,anagram1));
+        System.out.println(isAnagramStringBuilder(word3,anagram3));
+
+
+	}
+}
+
+---------------------------------------------------------------------------------------------------------------------
+
+	
+	
+	
+	
