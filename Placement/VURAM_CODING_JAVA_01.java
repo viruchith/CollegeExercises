c4d2cb7c462aefd3d3b8eea14481947d2c4601d5
Viruchith <58523045+viruchith@users.noreply.github.com>
2022-04-27 22:09:57 +0530

    Update VURAM_CODING_JAVA_01.java

--- Placement/VURAM_CODING_JAVA_01.java
+++ Placement/VURAM_CODING_JAVA_01.java
@@ -159,3 +159,33 @@ public class Main
         
 	}
 }
+
+
+/*
+Pattern
+
+1
+22
+333
+4444
+55555
+
+*/
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+        int n = 5;
+    
+       for(int i = 1 ; i<=n ; i++){
+           for(int j = 0; j<i ; j++){
+               System.out.print(i);
+           }
+           System.out.println();
+       }
+        
+	}
+}
+
