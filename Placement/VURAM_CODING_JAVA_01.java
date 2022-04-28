baf24f7628db74c21e65dffc1abc65b5334db87e
Viruchith <58523045+viruchith@users.noreply.github.com>
2022-04-28 06:23:27 +0530

    Update VURAM_CODING_JAVA_01.java

--- Placement/VURAM_CODING_JAVA_01.java
+++ Placement/VURAM_CODING_JAVA_01.java
@@ -243,3 +243,23 @@ public class Main
 	}
 }
 
+// -  For a given array even array add each pairs and place it in new array
+public class Main
+{
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5,6};
+		
+		int[] sumArr = new int[arr.length/2];
+		
+		int index = 0;
+		
+		for(int i = 1 ; i<arr.length;i+=2){
+		    sumArr[index++] = arr[i-1] + arr[i];
+		}
+		
+		for(int num : sumArr){
+		    System.out.print(num+" ");
+		}
+		
+	}
+}
