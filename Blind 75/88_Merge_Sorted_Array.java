aeef7ebfa0229163f5e0ffa1cad1eed561127c03
Viruchith <58523045+viruchith@users.noreply.github.com>
2022-08-04 20:29:43 +0530

    Create 88_Merge_Sorted_Array.java

---
+++ Blind 75/88_Merge_Sorted_Array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int last = m + n - 1;
+        
+        while(m>0 && n>0){
+            if(nums1[m-1]>nums2[n-1]){
+                nums1[last] = nums1[m-1];
+                m--;
+            }else{
+                nums1[last] = nums2[n-1];
+                n--;
+            }
+            last--;
+        }
+        
+      // Edge case copy remaining sorted from nums2 to nums1
+        while(n>0){
+            nums1[last] = nums2[n-1];
+            last--;
+            n--;
+        }
+        
+        
+    }
+}
